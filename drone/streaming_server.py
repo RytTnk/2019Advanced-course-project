b2c477f406b981eda7e6f4f422201fb28bdade22
K-Ishimori <56746321+K-Ishimori@users.noreply.github.com>
2019-12-13 17:40:00 +0900

    Server側をさらにいくつか関数化

--- drone/streaming_server.py
+++ drone/streaming_server.py
@@ -45,8 +45,10 @@ class streaming_server_utility:
         self.INDENT = '    '
 
         ## For control FPS
+        self.old_time = 0.0
         self.now_time = time.time()
-        self.old_time = self.now_time
+        self.fps = 0.0
+        self.delay_time = 0.0
 
         ## For D3Dshot module
         self.d = d3dshot.create(capture_output="numpy")
@@ -55,7 +57,6 @@ class streaming_server_utility:
         width = self.IMAGE_WIDTH
         height = self.IMAGE_HEIGHT
         self.d.capture(region=(x, y, x+width, y+height))
-        time.sleep(1)  # Capture is non-blocking so we wait explicitely
 
     #_____________________________________________________________________
 
@@ -66,6 +67,59 @@ class streaming_server_utility:
     ######################################################################
     def get_time_proc(self):
         self.now_time = time.time()
+    #_____________________________________________________________________
+
+    #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    # ----- Set time processing -----
+    ######################################################################
+    ## @brief      Set time processing
+    ######################################################################
+    def set_time_proc(self):
+        self.old_time = self.now_time
+    #_____________________________________________________________________
+
+    #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    # ----- Print FPS processing -----
+    ######################################################################
+    ## @brief      Print FPS processing
+    ######################################################################
+    def print_fps_proc(self):
+        print(self.fps)
+    #_____________________________________________________________________
+
+    #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    # ----- Print delay time processing -----
+    ######################################################################
+    ## @brief      Print delay time processing
+    ######################################################################
+    def print_delay_time_proc(self):
+        print(self.delay_time)
+    #_____________________________________________________________________
+
+    #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    # ----- Calc FPS processing -----
+    ######################################################################
+    ## @brief      Calc FPS processing
+    ######################################################################
+    def calc_fps_proc(self):
+        if self.now_time != self.old_time:
+            self.fps = 1.0 / (self.now_time - self.old_time)
+        else:
+            self.fps = 0.0
+    #_____________________________________________________________________
+
+    #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    # ----- Control FPS processing -----
+    ######################################################################
+    ## @brief      Control FPS processing
+    ######################################################################
+    def ctrl_fps_proc(self):
+        if self.now_time != self.old_time:
+            self.delay_time = max(0, (1 / self.FPS - (self.now_time - self.old_time)))
+        else:
+            self.delay_time = 0.0
+
+        time.sleep(self.delay_time)
     #_____________________________________________________________________
 
     #‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
@@ -85,12 +139,14 @@ class streaming_server_utility:
     ## @brief      Transmit prepare setting processing
     ######################################################################
     def transmit_prepare_set_proc(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#ソケットオブジェクト作成
-        self.s.bind((self.SERVER_IP, self.SERVER_PORT)) # サーバー側PCのipと使用するポート
-        print("接続待機中")
-        self.s.listen(1)                                # 接続要求を待機
-        self.soc, self.addr = self.s.accept()           # 要求が来るまでブロック
-        print(str(self.addr)+"と接続完了")
+        # Create socket comunication object
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.SERVER_IP, self.SERVER_PORT))
+
+        print("Wait for communication")
+        self.s.listen(1)
+        self.soc, self.addr = self.s.accept()
+        print("Start transmit to "+str(self.addr))
     #_____________________________________________________________________
 
 
@@ -128,48 +184,24 @@ class streaming_server_utility:
 ######################################################################
 def main_proc():
 
-    sc_utility = streaming_server_utility()
-
-    sc_utility.transmit_prepare_set_proc()
-
-    # 計測用
-    cnt = 0
-    ave = 0.0
-    old_time = 0
+    # Prepare socket communication
+    ss_utility = streaming_server_utility()
+    ss_utility.transmit_prepare_set_proc()
 
     while (True):
-        loop_start_time = time.time()
+        # Set asnd get timer counter
+        ss_utility.set_time_proc()
+        ss_utility.get_time_proc()
 
-        sc_utility.get_img_proc()
-        sc_utility.transmit_to_client_proc()
+        # Transmit image to client
+        ss_utility.get_img_proc()
+        ss_utility.transmit_to_client_proc()
 
-        # FPS制御
-        #time.sleep(max(0, 1 / FPS - (time.time() - loop_start_time)))
-
-        #sleep_time = max(0, 1 / FPS - (time.time() - loop_start_time))
-        #print(sleep_time)
-
-        now_time = time.time()
-
-        cnt = cnt + 1
-        if now_time != old_time:
-            ave = ave + 1.0/(now_time-old_time)
-        cycle = 10
-
-        if cnt == cycle:
-            print(ave/cycle)
-            ave = 0.0
-            cnt = 0
-
-        #print(1.0/(now_time-old_time))
-        old_time = now_time
-
-        k = cv2.waitKey(1)         #↖
-        if k== 13 :                #←　ENTERキーで終了
-            break                  #↙
-
-    #cam.releace()                  #カメラオブジェクト破棄
-
+        # Cacl and control fps
+        ss_utility.calc_fps_proc()
+        ss_utility.ctrl_fps_proc()
+        #ss_utility.print_fps_proc()
+        ss_utility.print_delay_time_proc()
 
 if __name__ == '__main__':
     main_proc()
